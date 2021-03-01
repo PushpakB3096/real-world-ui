a37a0f6ca475113a3eb94ab06be82c3d894c624b
PushpakB3096 <rtpushpak@gmail.com>
2021-03-01 21:15:18 +0530

    Add links in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,9 +5,10 @@
       <ul class="nav navbar-nav pull-xs-right">
         <li class="nav-item">
           <!-- Add "active" class when you're on that page" -->
-          <a class="nav-link active" href="">Home</a>
+          <router-link class="nav-link active" to="/">Home</router-link>
         </li>
-        <li class="nav-item">
+        <!-- uncomment below lines when the link is ready -->
+        <!-- <li class="nav-item">
           <a class="nav-link" href="">
             <i class="ion-compose"></i>&nbsp;New Post
           </a>
@@ -16,9 +17,13 @@
           <a class="nav-link" href="">
             <i class="ion-gear-a"></i>&nbsp;Settings
           </a>
+        </li> -->
+        <!-- TODO: Conditionally render only one of the below links based on user login status -->
+        <li class="nav-item">
+          <router-link class="nav-link" to="register">Sign Up</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="">Sign up</a>
+          <router-link class="nav-link" to="login">Login</router-link>
         </li>
       </ul>
     </div>
