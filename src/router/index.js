bf67dd5da86e04023f19d0a8d4bb7356ae71ce4a
PushpakB3096 <rtpushpak@gmail.com>
2021-03-01 20:57:53 +0530

    Add register and login page

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,18 @@ const routes = [
     path: "/",
     name: "Home",
     component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue")
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue")
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Register.vue")
   }
 ];
 
