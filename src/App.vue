856c8a0321d6ebbdbf01d9bd4fb81f3e014ddaac
PushpakB3096 <rtpushpak@gmail.com>
2021-03-01 21:01:32 +0530

    Use navbar in home page without style

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
+  <div id="app">
+    <Navbar></Navbar>
   </div>
   <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Navbar from "@/components/Navbar.vue";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
