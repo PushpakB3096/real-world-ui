f15d72b4fe993c5d0b6ca351dbad6c6630752d4a
PushpakB3096 <rtpushpak@gmail.com>
2021-03-04 19:57:00 +0530

    Add user authentication related endpoints

---
+++ src/store/modules/users.js
@@ -0,0 +1,39 @@
+import { API, setToken } from "../api";
+
+export default {
+  namespaced: true,
+  state: {
+    // 'user 'will hold the current logged in user
+    user: null,
+    // 'profile' will hold the user whose profile I am looking at
+    profile: null,
+  },
+  getters: {},
+  mutations: {
+    setUser(state, payload) {
+      state.user = payload;
+    },
+  },
+  actions: {
+    getUser: async function({ commit }) {
+      const user = await API.get("/user");
+      commit("setUser", user);
+    },
+    loginUser: async function({ commit }, { email, password }) {
+      const response = await API.post("/users/login", {
+        user: {
+          email,
+          password,
+        },
+      });
+
+      if (response.data.user) {
+        // gets the jwt token from the response and stores it in the auth header
+        const token = response.data.user.token;
+        setToken(token);
+        // setting the logged in user as the current user
+        commit("setUser", response.data.user);
+      }
+    },
+  },
+};
